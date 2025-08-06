7091c6d51064dcb7b9f1b1a4801aa376534b2c4d
viniciusdutra314 <dutravinisousa2@usp.br>
2025-08-06 13:56:27 +0000

    feat: simple radioactive counter

--- aquecimento/src/main.rs
+++ aquecimento/src/main.rs
@@ -1,6 +1,17 @@
-
-
+use rand::prelude::*;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
-    println!("Hello, world!");
+    let mut n = 100_000;
+    let mut rng=rand::rng();
+    let mut file_results=File::create("results.dat").unwrap();
+    while n > 0 {
+        writeln!(file_results,"{}",n);
+        for _ in 1..=n {
+            if rng.random_bool(0.1){
+                n-=1;
+            }
+        }
+    }
 }
