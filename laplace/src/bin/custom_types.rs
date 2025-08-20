9b6c2fc412edd8f5f46871288e97f8bfaee08991
viniciusdutra314 <dutravinisousa2@usp.br>
2025-08-20 12:59:42 +0000

    feat: unsafe unitialized type

--- laplace/src/bin/custom_types.rs
+++ laplace/src/bin/custom_types.rs
@@ -1,14 +1,55 @@
+use std::{mem::MaybeUninit, vec};
+use contracts::*;
 
-struct Symmetrical2DMatrix<T>{
-    data: Vec<T>,
+
+struct SymmetricMatrixUninit<T:num_traits::Num +Copy>{
+    data: Box<[MaybeUninit<T>]>,
     dimension:usize
+} 
+
+impl<T:num_traits::Num +Copy> SymmetricMatrixUninit<T>{
+    pub fn new(n:usize) ->Self {
+        return Self{data:Box::new_uninit_slice(n*(n+1)/2),dimension:n};
+    }
+    pub unsafe fn set_uncheked(&mut self,i:usize,j:usize,value:T){
+        if j>i {
+            unsafe {
+                let _= *self.data.get_unchecked_mut(j*(j+1)/2 +i).write(value);
+            }
+        }
+        else {
+            unsafe {
+                let _= *self.data.get_unchecked_mut(i*(i+1)/2 +j).write(value);
+            }
+        }
+    }
+    #[requires(i<self.dimension && j<self.dimension,"The element should be inside the matrix")]
+    pub fn set(&mut self,i:usize,j:usize,value:T){
+        unsafe {
+            self.set_uncheked(i, j, value);
+        }
+    }
+
+    pub unsafe fn has_initialized(self) -> SymmetricMatrix<T>{
+        unsafe {
+            return SymmetricMatrix { data: (self.data.assume_init()), dimension: (self.dimension) };
+        }
+    }
+
 }
 
-impl<T:num_traits::Num + Copy> Symmetrical2DMatrix<T> {
-    pub fn new(size:usize) -> Self {
-        let data = vec![T::zero(); (size * (size+1))/2];
-        return Self{data:data,dimension:size};
+
+struct SymmetricMatrix<T>{
+    data: Box<[T]>,
+    dimension:usize,
+}
+
+impl<T:num_traits::Num + Copy> SymmetricMatrix<T> {
+    pub fn new(n:usize,fill_value:T) -> Self {
+        return Self{data:vec![fill_value;n*(n+1)/2].into_boxed_slice(),dimension:n};
     }
+
+
     pub fn get_dimension(&self)->usize{
         return self.dimension;
     }
@@ -26,10 +67,8 @@ impl<T:num_traits::Num + Copy> Symmetrical2DMatrix<T> {
         }
     }
 
+    #[requires(i<self.dimension && j<self.dimension,"The element should be inside the matrix")]
     pub fn get(&self, i:usize,j:usize) -> T{
-        if i>=self.dimension || j>=self.dimension{
-            panic!("Out of bounds error, ({},{}) {}x{}",i,j,self.dimension,self.dimension);
-        }
         unsafe{
             return self.get_unchecked(i, j);
         };
@@ -48,11 +87,8 @@ impl<T:num_traits::Num + Copy> Symmetrical2DMatrix<T> {
         }
 
     }
+    #[requires(i<self.dimension && j<self.dimension,"The element should be inside the matrix")]
     pub fn set(&mut self,i:usize,j:usize,value:T){
-        if i>=self.dimension || j>=self.dimension{
-            panic!("Out of bounds error, ({},{}) {}x{}",i,j,self.dimension,self.dimension);
-        }
-        
         unsafe {
             self.set_uncheked(i, j, value);
         }
@@ -67,7 +103,7 @@ mod tests {
 
     #[test]
     fn test_initialization() {
-        let matrix=Symmetrical2DMatrix::<i32>::new(2);
+        let matrix=SymmetricMatrix::<i32>::new(2,0);
         assert_eq!(matrix.get(0, 0),0);
         assert_eq!(matrix.get(0, 1),0);
         assert_eq!(matrix.get(1, 0),0);
@@ -75,22 +111,24 @@ mod tests {
     }
     #[test]
     fn test_set(){
-        let mut matrix=Symmetrical2DMatrix::<f64>::new(3);
+        let mut uninit_matrix=SymmetricMatrixUninit::<f64>::new(3);
         for i in 0..3{
             for j in 0..3{
-                matrix.set(i, j, (i*j) as f64);
+                uninit_matrix.set(i, j, (i*j) as f64);
             }
         }
+        let init_matrix:SymmetricMatrix::<f64>=unsafe {uninit_matrix.has_initialized()};
+
         for i in 0..3{
             for j in 0..3{
-                assert_eq!(matrix.get(i, j),(i*j) as f64);
+                assert_eq!(init_matrix.get(i, j),(i*j) as f64);
             }
         }
     }
 
     #[test]
     fn test_multiple_invalid_indices() {
-        let matrix = Symmetrical2DMatrix::<i8>::new(10);
+        let matrix = SymmetricMatrix::<i8>::new(10,0);
         for i in [10,11,12] {
             for j in [4,5,6]{
                 let result_a = std::panic::catch_unwind(|| matrix.get(i, j));
@@ -105,7 +143,7 @@ mod tests {
     
     #[test]
     fn test_symmmetry(){
-        let mut matrix=Symmetrical2DMatrix::<i128>::new(10);
+        let mut matrix=SymmetricMatrix::<i128>::new(10,0);
         for i in 0..matrix.get_dimension() {
             for j in 0..matrix.get_dimension(){
                 unsafe {
