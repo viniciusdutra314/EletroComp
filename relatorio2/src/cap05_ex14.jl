32720091ff5360de4e5d57f9ba773c686bf1d1c1
Vinícius Dutra <dutravinisousa2@usp.br>
2025-11-11 12:47:58 +0000

    feat: solenoide

--- relatorio2/src/cap05_ex14.jl
+++ relatorio2/src/cap05_ex14.jl
@@ -1,3 +1,16 @@
-function integrate(f,a,b,n)
+include("magnetic_field.jl")
+using Plots
 
-end 
+const f=1
+const n=1000
+const r=1
+const θf=20.0
+const n_voltas=floor(Int64,θf*f)
+curve(θ) = (r*cos(2π*θ*f),r*sin(2π*θ*f),θ)
+fig=plot3d(curve.(range(0,θf,n)),label="Solenoide n_voltas=$(n_voltas)")
+r=SVector(curve(θf/2)...)
+
+const I=1.0
+B_expected=(μ0*n_voltas*I)/θf
+#B=magnetic_field(r,I,curve,0.0,θf,n)
+savefig(fig,"teste.png")
